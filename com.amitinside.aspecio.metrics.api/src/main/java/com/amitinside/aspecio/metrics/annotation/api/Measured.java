71eb7e57a5612b3cd19d91be6fe907efc68555d0
Amit Kumar Mondal <admin@amitinside.com>
2021-07-14 06:11:23 +0200

    Added Javadoc

--- com.amitinside.aspecio.metrics.api/src/main/java/com/amitinside/aspecio/metrics/annotation/api/Measured.java
+++ com.amitinside.aspecio.metrics.api/src/main/java/com/amitinside/aspecio/metrics/annotation/api/Measured.java
@@ -4,6 +4,9 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Retention;
 
+/**
+ * Annotation to be used to intercept metrics information from the annotated class
+ */
 @Retention(RUNTIME)
 public @interface Measured {
 

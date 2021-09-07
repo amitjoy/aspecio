73ead5f64b519a8678a077be6a97fcc040c31900
Amit Kumar Mondal <admin@amitinside.com>
2021-09-07 15:01:23 +0200

    Added Missing Copyright Headers

--- com.amitinside.aspecio.examples/src/main/java/com/amitinside/aspecio/examples/aspect/metric/internal/AllMetricInterceptorImpl.java
+++ com.amitinside.aspecio.examples/src/main/java/com/amitinside/aspecio/examples/aspect/metric/internal/AllMetricInterceptorImpl.java
@@ -1,11 +1,29 @@
+/*******************************************************************************
+ * Copyright 2021 Amit Kumar Mondal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
 package com.amitinside.aspecio.examples.aspect.metric.internal;
 
 import java.util.concurrent.TimeUnit;
+
 import org.osgi.service.component.annotations.Component;
 import org.osgi.util.promise.Promise;
+
 import com.amitinside.aspecio.annotations.api.Aspect;
 import com.amitinside.aspecio.examples.aspect.metric.MetricAspect;
 import com.google.common.base.Stopwatch;
+
 import io.primeval.reflex.proxy.CallContext;
 import io.primeval.reflex.proxy.Interceptor;
 import io.primeval.reflex.proxy.handler.InterceptionHandler;
@@ -17,8 +35,8 @@ public final class AllMetricInterceptorImpl implements Interceptor {
     @Override
     public <T, E extends Throwable> T onCall(final CallContext callContext, final InterceptionHandler<T> handler)
             throws E {
-        final Stopwatch started    = Stopwatch.createStarted();
-        final String    methodName = callContext.target.getName() + "::" + callContext.method.getName();
+        final Stopwatch started = Stopwatch.createStarted();
+        final String methodName = callContext.target.getName() + "::" + callContext.method.getName();
 
         final boolean async = callContext.method.getReturnType() == Promise.class;
 
--- com.amitinside.aspecio.examples/src/main/java/com/amitinside/aspecio/examples/aspect/metric/internal/AnnotatedMetricInterceptorImpl.java
+++ com.amitinside.aspecio.examples/src/main/java/com/amitinside/aspecio/examples/aspect/metric/internal/AnnotatedMetricInterceptorImpl.java
@@ -1,12 +1,30 @@
+/*******************************************************************************
+ * Copyright 2021 Amit Kumar Mondal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
 package com.amitinside.aspecio.examples.aspect.metric.internal;
 
 import java.util.concurrent.TimeUnit;
+
 import org.osgi.service.component.annotations.Component;
 import org.osgi.util.promise.Promise;
+
 import com.amitinside.aspecio.annotations.api.Aspect;
 import com.amitinside.aspecio.examples.aspect.metric.MetricAspect;
 import com.amitinside.aspecio.examples.aspect.metric.Timed;
 import com.google.common.base.Stopwatch;
+
 import io.primeval.reflex.proxy.CallContext;
 import io.primeval.reflex.proxy.annotation.AnnotationInterceptor;
 import io.primeval.reflex.proxy.handler.InterceptionHandler;
@@ -19,8 +37,8 @@ public final class AnnotatedMetricInterceptorImpl implements AnnotationIntercept
     public <T, E extends Throwable> T onCall(final Timed annotation, final CallContext callContext,
             final InterceptionHandler<T> handler) {
 
-        final Stopwatch started    = Stopwatch.createStarted();
-        final String    methodName = callContext.target.getName() + "::" + callContext.method.getName();
+        final Stopwatch started = Stopwatch.createStarted();
+        final String methodName = callContext.target.getName() + "::" + callContext.method.getName();
 
         final boolean async = callContext.method.getReturnType() == Promise.class;
 

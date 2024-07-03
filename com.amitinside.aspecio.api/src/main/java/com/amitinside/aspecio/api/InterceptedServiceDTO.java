801a033f026e6781251d0b1e787d323fa03692ef
Amit Kumar Mondal <admin@amitinside.com>
2024-07-03 13:20:19 +0200

    Added comments

--- com.amitinside.aspecio.api/src/main/java/com/amitinside/aspecio/api/InterceptedServiceDTO.java
+++ com.amitinside.aspecio.api/src/main/java/com/amitinside/aspecio/api/InterceptedServiceDTO.java
@@ -2,14 +2,14 @@
  * Copyright 2021-2024 Amit Kumar Mondal
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License.  You may obtain a copy
+ * use this file except in compliance with the License. You may obtain a copy
  * of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
  ******************************************************************************/
@@ -21,20 +21,34 @@ import java.util.Set;
 import org.osgi.dto.DTO;
 
 /**
- * The data transfer object describing a service candidate to interception by
- * Aspecio, along with the Aspects it define and its status.
+ * The data transfer object describing a service candidate for interception by
+ * Aspecio, along with the aspects it defines and its status.
  *
  * @NotThreadSafe
  */
 public class InterceptedServiceDTO extends DTO {
 
+	/** The unique identifier of the service. */
 	public long serviceId;
-	public long bundleId;
-	public boolean published;
-	public List<String> objectClass;
-	public Set<String> satisfiedAspects;
-	public Set<String> requiredAspects;
-	public Set<String> optionalAspects;
-	public Set<String> unsatisfiedRequiredAspects;
 
+	/** The unique identifier of the bundle. */
+	public long bundleId;
+
+	/** Indicates if the service is published. */
+	public boolean published;
+
+	/** The list of object classes implemented by the service. */
+	public List<String> objectClass;
+
+	/** The set of aspects that are satisfied for this service. */
+	public Set<String> satisfiedAspects;
+
+	/** The set of aspects that are required for this service. */
+	public Set<String> requiredAspects;
+
+	/** The set of optional aspects for this service. */
+	public Set<String> optionalAspects;
+
+	/** The set of required aspects that are not satisfied for this service. */
+	public Set<String> unsatisfiedRequiredAspects;
 }

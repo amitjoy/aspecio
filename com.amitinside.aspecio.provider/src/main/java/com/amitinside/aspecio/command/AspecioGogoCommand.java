8869305f0b500800846092e79ade3629174c8c1b
Amit Kumar Mondal <admin@amitinside.com>
2024-07-11 10:01:07 +0200

    Updated Gogo Command to return strings instead

--- com.amitinside.aspecio.provider/src/main/java/com/amitinside/aspecio/command/AspecioGogoCommand.java
+++ com.amitinside.aspecio.provider/src/main/java/com/amitinside/aspecio/command/AspecioGogoCommand.java
@@ -17,10 +17,8 @@ package com.amitinside.aspecio.command;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
-
 import com.amitinside.aspecio.api.Aspecio;
 import com.amitinside.aspecio.api.AspectDTO;
 import com.amitinside.aspecio.api.InterceptedServiceDTO;
@@ -28,78 +26,83 @@ import com.amitinside.aspecio.api.InterceptorDTO;
 
 public final class AspecioGogoCommand {
 
-	private final BundleContext bundleContext;
-	private final Aspecio aspecio;
+    private final BundleContext bundleContext;
+    private final Aspecio aspecio;
 
-	public AspecioGogoCommand(final BundleContext bundleContext, final Aspecio aspecio) {
-		this.bundleContext = bundleContext;
-		this.aspecio = aspecio;
-	}
+    public AspecioGogoCommand(final BundleContext bundleContext, final Aspecio aspecio) {
+        this.bundleContext = bundleContext;
+        this.aspecio = aspecio;
+    }
 
-	// Gogo command "aspecio:aspects"
-	public void aspects() {
-		for (final String aspectName : aspecio.getRegisteredAspects()) {
-			System.out.println("* " + aspectName);
-			final Optional<AspectDTO> aspectDescription = aspecio.getAspectDescription(aspectName);
-			if (!aspectDescription.isPresent()) {
-				System.out.println(" ...?! Err, that aspect just went away!");
-				continue;
-			}
-			final AspectDTO description = aspectDescription.get();
-			printInterceptorDescription("[ --- ACTIVE --- ]", description.interceptor);
-			int i = 1;
-			for (final InterceptorDTO id : description.backupInterceptors) {
-				printInterceptorDescription("[ alternative #" + i + " ]", id);
-				i++;
-			}
-		}
-	}
+    // Gogo command "aspecio:aspects"
+    public String aspects() {
+        StringBuilder output = new StringBuilder();
+        for (final String aspectName : aspecio.getRegisteredAspects()) {
+            output.append("* ").append(aspectName).append("\n");
+            final Optional<AspectDTO> aspectDescription = aspecio.getAspectDescription(aspectName);
+            if (!aspectDescription.isPresent()) {
+                output.append(" ...?! Err, that aspect just went away!\n");
+                continue;
+            }
+            final AspectDTO description = aspectDescription.get();
+            output.append(printInterceptorDescription("[ --- ACTIVE --- ]", description.interceptor));
+            int i = 1;
+            for (final InterceptorDTO id : description.backupInterceptors) {
+                output.append(printInterceptorDescription("[ alternative #" + i + " ]", id));
+                i++;
+            }
+        }
+        return output.toString();
+    }
 
-	private void printInterceptorDescription(final String marker, final InterceptorDTO interceptorDescription) {
-		final String shift = String.format("%" + (marker.length() + 3) + "s", "");
-		System.out.println("  " + marker + " Service ID " + interceptorDescription.serviceId + ", class "
-				+ interceptorDescription.interceptorClass.getName() + ", extra properties: "
-				+ interceptorDescription.extraProperties);
-		final long serviceBundleId = interceptorDescription.bundleId;
-		final Bundle bundle = bundleContext.getBundle(serviceBundleId);
-		System.out.println(shift + "Provided by: " + bundle.getSymbolicName() + " " + bundle.getVersion() + " ["
-				+ serviceBundleId + "]");
-		System.out.println();
-	}
+    private String printInterceptorDescription(final String marker, final InterceptorDTO interceptorDescription) {
+        StringBuilder output = new StringBuilder();
+        final String shift = String.format("%" + (marker.length() + 3) + "s", "");
+        output.append("  ").append(marker).append(" Service ID ").append(interceptorDescription.serviceId)
+              .append(", class ").append(interceptorDescription.interceptorClass.getName())
+              .append(", extra properties: ").append(interceptorDescription.extraProperties).append("\n");
+        final long serviceBundleId = interceptorDescription.bundleId;
+        final Bundle bundle = bundleContext.getBundle(serviceBundleId);
+        output.append(shift).append("Provided by: ").append(bundle.getSymbolicName()).append(" ")
+              .append(bundle.getVersion()).append(" [").append(serviceBundleId).append("]\n\n");
+        return output.toString();
+    }
 
-	// Gogo command "aspecio:woven"
-	public void woven() {
-		final List<InterceptedServiceDTO> interceptedServices = aspecio.getInterceptedServices();
-		printWoven(interceptedServices);
-	}
+    // Gogo command "aspecio:woven"
+    public String woven() {
+        final List<InterceptedServiceDTO> interceptedServices = aspecio.getInterceptedServices();
+        return printWoven(interceptedServices);
+    }
 
-	public void woven(final String objectClass) {
-		final List<InterceptedServiceDTO> interceptedServices = aspecio.getInterceptedServices(objectClass);
-		printWoven(interceptedServices);
-	}
+    public String woven(final String objectClass) {
+        final List<InterceptedServiceDTO> interceptedServices = aspecio.getInterceptedServices(objectClass);
+        return printWoven(interceptedServices);
+    }
 
-	private void printWoven(final List<InterceptedServiceDTO> interceptedServices) {
-		int i = 0;
-		final int shiftSize = 4 + interceptedServices.size() / 10;
-		final String shift = String.format("%" + shiftSize + "s", "");
-		for (final InterceptedServiceDTO mws : interceptedServices) {
-			System.out.println("[" + i + "] Service ID: " + mws.serviceId + ", objectClass: " + mws.objectClass);
-			System.out.println(
-					shift + "Required Aspects: " + mws.requiredAspects + ", Optional Aspects: " + mws.optionalAspects);
-			final long serviceBundleId = mws.bundleId;
-			final Bundle bundle = bundleContext.getBundle(serviceBundleId);
-			System.out.println(shift + "Provided by: " + bundle.getSymbolicName() + " " + bundle.getVersion() + " ["
-					+ serviceBundleId + "]");
-			final boolean satisfied = mws.published;
-			System.out.println(shift + "Satisfied: " + satisfied);
-			if (!satisfied) {
-				System.out.println(shift + "Missing Required Aspects: " + mws.unsatisfiedRequiredAspects);
-			} else {
-				System.out.println(shift + "Active Aspects: " + mws.satisfiedAspects);
-			}
-			System.out.println();
-			i++;
-		}
-	}
-
+    private String printWoven(final List<InterceptedServiceDTO> interceptedServices) {
+        StringBuilder output = new StringBuilder();
+        int i = 0;
+        final int shiftSize = 4 + interceptedServices.size() / 10;
+        final String shift = String.format("%" + shiftSize + "s", "");
+        for (final InterceptedServiceDTO mws : interceptedServices) {
+            output.append("[").append(i).append("] Service ID: ").append(mws.serviceId)
+                  .append(", objectClass: ").append(mws.objectClass).append("\n");
+            output.append(shift).append("Required Aspects: ").append(mws.requiredAspects)
+                  .append(", Optional Aspects: ").append(mws.optionalAspects).append("\n");
+            final long serviceBundleId = mws.bundleId;
+            final Bundle bundle = bundleContext.getBundle(serviceBundleId);
+            output.append(shift).append("Provided by: ").append(bundle.getSymbolicName()).append(" ")
+                  .append(bundle.getVersion()).append(" [").append(serviceBundleId).append("]\n");
+            final boolean satisfied = mws.published;
+            output.append(shift).append("Satisfied: ").append(satisfied).append("\n");
+            if (!satisfied) {
+                output.append(shift).append("Missing Required Aspects: ").append(mws.unsatisfiedRequiredAspects).append("\n");
+            } else {
+                output.append(shift).append("Active Aspects: ").append(mws.satisfiedAspects).append("\n");
+            }
+            output.append("\n");
+            i++;
+        }
+        return output.toString();
+    }
 }

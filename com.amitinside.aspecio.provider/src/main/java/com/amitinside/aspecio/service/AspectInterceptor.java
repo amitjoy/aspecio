747c0e4f693e7ac270a086d53b3f3a90773b0bc3
Amit Kumar Mondal <admin@amitinside.com>
2024-07-15 10:20:23 +0200

    Added space formatting to AspectInterceptor

--- com.amitinside.aspecio.provider/src/main/java/com/amitinside/aspecio/service/AspectInterceptor.java
+++ com.amitinside.aspecio.provider/src/main/java/com/amitinside/aspecio/service/AspectInterceptor.java
@@ -2,17 +2,18 @@
  * Copyright 2022-2024 Amit Kumar Mondal
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License.  You may obtain a copy
+ * use this file except in compliance with the License. You may obtain a copy
  * of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
  ******************************************************************************/
+
 package com.amitinside.aspecio.service;
 
 import static java.util.Objects.requireNonNull;
@@ -26,45 +27,40 @@ import io.primeval.reflex.proxy.Interceptor;
 
 public final class AspectInterceptor implements Comparable<AspectInterceptor> {
 
-	public final String aspect;
-	public final Interceptor interceptor;
-	public final ServiceReference<?> serviceRef;
-	public final int serviceRanking;
-	public final Set<String> extraProperties;
+    public final String aspect;
+    public final Interceptor interceptor;
+    public final ServiceReference<?> serviceRef;
+    public final int serviceRanking;
+    public final Set<String> extraProperties;
 
-	public AspectInterceptor(final String aspect, final Interceptor interceptor, final ServiceReference<?> serviceRef,
-			final int serviceRanking, final Set<String> extraProperties) {
-		requireNonNull(aspect, "'Aspect cannot be null'");
-		requireNonNull(interceptor, "'Interceptor cannot be null'");
-		requireNonNull(serviceRef, "'Service reference cannot be null'");
+    public AspectInterceptor(final String aspect, final Interceptor interceptor, final ServiceReference<?> serviceRef,
+                             final int serviceRanking, final Set<String> extraProperties) {
+        this.aspect = requireNonNull(aspect, "'Aspect cannot be null'");
+        this.interceptor = requireNonNull(interceptor, "'Interceptor cannot be null'");
+        this.serviceRef = requireNonNull(serviceRef, "'Service reference cannot be null'");
+        this.serviceRanking = serviceRanking;
+        this.extraProperties = extraProperties;
+    }
 
-		this.aspect = aspect;
-		this.interceptor = interceptor;
-		this.serviceRef = serviceRef;
-		this.serviceRanking = serviceRanking;
-		this.extraProperties = extraProperties;
-	}
+    @Override
+    public int compareTo(final AspectInterceptor o) {
+        return serviceRef.compareTo(o.serviceRef);
+    }
 
-	@Override
-	public int compareTo(final AspectInterceptor o) {
-		return serviceRef.compareTo(o.serviceRef);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(serviceRef);
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(serviceRef);
-	}
-
-	@Override
-	public boolean equals(final Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null || !(obj instanceof AspectInterceptor)) {
-			return false;
-		}
-		final AspectInterceptor other = (AspectInterceptor) obj;
-		return Objects.equals(serviceRef, other.serviceRef);
-	}
-
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || !(obj instanceof AspectInterceptor)) {
+            return false;
+        }
+        final AspectInterceptor other = (AspectInterceptor) obj;
+        return Objects.equals(serviceRef, other.serviceRef);
+    }
 }

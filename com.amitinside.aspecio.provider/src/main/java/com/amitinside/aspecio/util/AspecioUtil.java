55c3e368ddf6e65490a6fe16f41024df8c3febfb
Amit Kumar Mondal <admin@amitinside.com>
2024-07-17 10:32:15 +0200

    Refactored utility class

--- com.amitinside.aspecio.provider/src/main/java/com/amitinside/aspecio/util/AspecioUtil.java
+++ com.amitinside.aspecio.provider/src/main/java/com/amitinside/aspecio/util/AspecioUtil.java
@@ -2,7 +2,7 @@
  * Copyright 2022-2024 Amit Kumar Mondal
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License.  You may obtain a copy
+ * use this file except in compliance with the License. You may obtain a copy
  * of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
@@ -35,39 +35,33 @@ public final class AspecioUtil {
 	public static <T> T trust(final Callable<T> block) {
 		try {
 			return block.call();
-		} catch (final RuntimeException e) {
+		} catch (RuntimeException e) {
 			throw e;
-		} catch (final Exception e) {
+		} catch (Exception e) {
 			throw Exceptions.duck(e);
 		}
 	}
 
 	public static String asString(final Object propObj) {
-		String res;
-		if (propObj == null) {
-			res = null;
+		if (propObj instanceof String) {
+			return (String) propObj;
 		} else if (propObj instanceof String[] && ((String[]) propObj).length == 1) {
-			res = ((String[]) propObj)[0];
-		} else if (propObj instanceof String) {
-			res = (String) propObj;
-		} else {
-			throw new IllegalArgumentException("Can only convert properties of type String or String[] of size 1");
+			return ((String[]) propObj)[0];
+		} else if (propObj == null) {
+			return null;
 		}
-		return res;
+		throw new IllegalArgumentException("Can only convert properties of type String or String[] of size 1");
 	}
 
 	public static String[] asStringArray(final Object propObj) {
-		String[] res = null;
-		if (propObj == null) {
-			res = new String[0];
+		if (propObj instanceof String) {
+			return new String[] { (String) propObj };
 		} else if (propObj instanceof String[]) {
-			res = (String[]) propObj;
-		} else if (propObj instanceof String) {
-			res = new String[] { (String) propObj };
-		} else {
-			throw new IllegalArgumentException("Can only convert properties of type String or String[]");
+			return (String[]) propObj;
+		} else if (propObj == null) {
+			return new String[0];
 		}
-		return res;
+		throw new IllegalArgumentException("Can only convert properties of type String or String[]");
 	}
 
 	public static long asLong(final Object propObj) {
@@ -78,23 +72,22 @@ public final class AspecioUtil {
 	}
 
 	public static int asInt(final Object propObj, final int defaultValue) {
-		return propObj instanceof Integer ? (Integer) propObj : defaultValue;
+		return (propObj instanceof Integer) ? (Integer) propObj : defaultValue;
 	}
 
 	public static <T> T firstOrNull(final SortedSet<T> set) {
-		return set == null || set.isEmpty() ? null : set.first();
+		return (set == null || set.isEmpty()) ? null : set.first();
 	}
 
 	public static <T> Set<T> asSet(final Collection<T> source) {
 		return source.stream().collect(toSet());
 	}
 
-	public static <T> Set<String> asSet(final T source) {
+	public static Set<String> asSet(final Object source) {
 		return Stream.of(asStringArray(source)).collect(toSet());
 	}
 
-	public static <T> List<String> asList(final T source) {
+	public static List<String> asList(final Object source) {
 		return Stream.of(asStringArray(source)).collect(toList());
 	}
-
 }
